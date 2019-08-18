acd956c26e83ab080d2bc6333510faee5c5fa05e
Raymond <35454616+raymondng76@users.noreply.github.com>
2019-08-18 11:19:01 +0800

    add chatbot utility functions

--- SystemCode/Level_Up_Chatbot/Level_Up_App/chatbot_util.py
+++ SystemCode/Level_Up_Chatbot/Level_Up_App/chatbot_util.py
@@ -1,4 +1,7 @@
-from Level_Up_App.models import CareerSkills, CareerPosition, Skill
+from django.db.models import Count
+from Level_Up_App.models import CareerSkills, CareerPosition, Skill, Job, GenericInfo
+from Level_Up_App.genericjobinfo import *
+
 
 def getJobCompetency(jobtitle):
     jobcompetency = []
@@ -7,3 +10,124 @@ def getJobCompetency(jobtitle):
     for skill in filterCareerPos.skillRequired.all():
         jobcompetency.append(str(skill))
     return jobcompetency
+
+def getHighestDemandJob():
+    highest = 0
+    allcareerpos = CareerPosition.objects.all()
+    hdjob = allcareerpos[0].name
+    for pos in allcareerpos:
+        count = Job.objects.filter(name=pos).count()
+        if count > highest:
+            highest = count
+            hdjob = pos.name
+    return hdjob
+
+def getJobEducationLevel(jobtitle):
+    return str(queryGenericInfo(jobtitle).eduLvl)
+
+def getJobSalary(jobtitle):
+    return str(queryGenericInfo(jobtitle).salaryRange)
+
+def getJobDescription(jobtitle):
+    return str(queryGenericInfo(jobtitle).description)
+
+def getJobMinYearsExperience(jobtitle):
+    return str(queryGenericInfo(jobtitle).minYears)
+
+def queryGenericInfo(jobtitle):
+    careerpos = CareerPosition.objects.get(name=jobtitle)
+    return GenericInfo.objects.get(title=careerpos)
+
+
+def getJobDescriptionTemp(jobtitle):
+    title = jobtitle.lower()
+    if title == 'intern':
+        return getInternDescription()
+    if title == 'engineer':
+        return getEngineerDescription()
+    if title == 'senior engineer':
+        return getSeniorEngineerDescription()
+    if title == 'software engineer':
+        return getSoftwareEngineerDescription()
+    if title == 'software developer':
+        return getSoftwareDeveloperDescription()
+    if title == 'analyst':
+        return getAnalystDescription()
+    if title == 'sales engineer':
+        return getSalesEngineerDescription()
+    if title == 'business analyst':
+        return getBusinessAnalystDescription()
+    if title == 'principle engineer':
+        return getPrincipalEngineerDescription()
+    if title == 'senior software engineer':
+        return getSeniorSoftwareEngineerDescription()
+    if title == 'senior software developer':
+        return getSeniorSoftwareDeveloperDescription()
+    if title == 'solution architect':
+        return getSolutionArchitectDescription()
+    if title == 'consultant':
+        return getConsultantDescription()
+    if title == 'senior analyst':
+        return getSeniorAnalystDescription()
+    if title == 'senior sales engineer':
+        return getSeniorSalesEngineerDescription()
+    if title == 'senior business analyst':
+        return getSeniorBusinessAnalystDescription()
+    if title == 'principle software engineer':
+        return getPrincipalSoftwareEngineerDescription()
+    if title == 'senior solution architect':
+        return getSeniorSolutionArchitectDescription()
+    if title == 'senior consultant':
+        return getSeniorConsultantDescription()
+    if title == 'technical lead':
+        return getTechnicalLeadDescription()
+    if title == 'project manager':
+        return getProjectManagerDescription()
+    if title == 'software manager':
+        return getSoftwareManagerDescription()
+    if title == 'sales manager':
+        return getSalesManagerDescription()
+    if title == 'senior project manager':
+        return getSeniorProjectManagerDescription()
+    if title == 'technical manager':
+        return getTechnicalManagerDescription()
+    if title == 'business manager':
+        return getBusinessManagerDescription()
+    if title == 'senior software manager':
+        return getSeniorSoftwareManagerDescription()
+    if title == 'senior sales manager':
+        return getSeniorSalesManagerDescription()
+    if title == 'senior technical manager':
+        return getSeniorTechnicalManagerDescription()
+    if title == 'programme manager':
+        return getProgrammeManagerDescription()
+    if title == 'sales director':
+        return getSalesDirectorDescription()
+    if title == 'professor':
+        return getProfessorDescription()
+    if title == 'director':
+        return getDirectorDescription()
+    if title == 'software director':
+        return getSoftwareDirectorDescription()
+    if title == 'head':
+        return getHeadDescription()
+    if title == 'senior sales director':
+        return getSeniorSalesDirectorDescription()
+    if title == 'senior director':
+        return getSeniorDirectorDescription()
+    if title == 'senior software director':
+        return getSeniorSoftwareDirectorDescription()
+    if title == 'senior head':
+        return getSeniorHeadDescription()
+    if title == 'chief technology officer' or title == 'cto':
+        return getCTODescription()
+    if title == 'vice president' or title == 'vp':
+        return getVPDescription()
+    if title == 'chief information officer' or title == 'cio':
+        return getCIODescription()
+    if title == 'president':
+        return getPresidentDescription()
+    if title == 'chief operating officer' or title == 'coo':
+        return getCOODescription()
+    if title == 'chief executive officer' or title == 'ceo':
+        return getCEODescription()
