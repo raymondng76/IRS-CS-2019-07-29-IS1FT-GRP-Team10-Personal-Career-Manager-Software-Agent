60fd95da5f3965f6f756fc8a578e53111c65ab08
ghSaint <52459036+ghSaint@users.noreply.github.com>
2019-08-20 15:59:00 +0800

    Update views.py

--- SystemCode/Level_Up_Chatbot/Level_Up_App/views.py
+++ SystemCode/Level_Up_Chatbot/Level_Up_App/views.py
@@ -103,8 +103,172 @@ def webhook(request):
         reply = {'fulfillmentText': 'This is Django test response from webhook. Action or Intent not found'}
     # return generated response
     return JsonResponse(reply, safe=False)
+###
+#Alfred
+###
 
+if intent_name == "A_GetCareerRoadMapInfo":
+        persona = "Curious Explorer"
+        resp_text = "The Career Road Map shows you a career path to achieve your career aspiration in the shortest time. It is generated based on anonymised data of real career advancement. Would you be interested to discover your career road map?"
+    elif intent_name == "A_GetCareerRoadMapInfo - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetCareerRoadMapInfo - no":
+        resp_text = "Okay what else can I do for you?"
+    elif intent_name == "A_GetHighestDemandJob":
+        #jobtitle = getHighestDemandJob()
+        resp_text =  f"Currently the highest demand job is {jobtitle}"
+    elif intent_name == "A_GetJobCompetency":
+        persona = "Go Getter"
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        #competency = getJobCompentency(jobInterestedIn)
+        resp_text =  f"{jobInterestedIn} requires the following competencies: {competency}. Would you be interested to see a road map on how to get there?"
+    elif intent_name == "A_GetJobCompetency - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobCompetency - no":
+        resp_text = "Okay what else can I do for you?"
+    elif intent_name == "A_GetJobDifference":
+        persona = "Go Getter"
+        jobtitle1 = req["queryResult"]["parameters"]["jobtitle1"]
+        jobtitle2 = req["queryResult"]["parameters"]["jobtitle2"]
+        print(jobtitle1)
+        print(jobtitle2)
+        jd1 = getJobDescription(jobtitle1)
+        jd2 = getJobDescription(jobtitle2)
+        resp_text = f"{jobtitle1} \n {jd1} \n {jobtitle2} \n {jd2} \n Which position are you more interested in?"
+        #getJobDescription(job1)
+        #getJobDescription(job2)
+    elif intent_name == "A_GetJobDifference - custom":
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        resp_text = "I see, would you like me to show you a road map on how you can get there?"
+    elif intent_name == "A_GetJobDifference - custom - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobDifference - custom - no":
+        resp_text = "Okay what else can I do for you?"
+    elif intent_name == "A_GetJobEducation":
+        persona = "Go Getter"
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        #education = getJobEducationLevel(jobInterestedIn)
+        resp_text =  f"{jobInterestedIn} requires {education}. Would you be interested to see a road map on how to get there?"
+    elif intent_name == "A_GetJobEducation - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobEducation - no":
+        resp_text = "Okay what else can I do for you?"
+    elif intent_name == "A_GetJobPath":
+        persona = "Go Getter"
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        resp_text = "I can help you with that! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobSalary":
+        persona = "Go Getter"
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        #salary = getJobSalary(jobInterestedIn)
+        #resp_text = f"On average, {jobInterestedIn} earns {salary} a month. Would you be interested to see a road map on how to get there?"
+    elif intent_name == "A_GetJobSalary - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobSalary - no":
+        resp_text = "Okay what else can I do for you?"
+    elif intent_name == "A_GetJobScope":
+        persona = "Go Getter"
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        #jd = getJobDescription(jobInterestedIn)
+        resp_text = f"Below is the job description of a {jobInterestedIn}: \n {jd}. \n Would you be interested to see a road map on how to get there?"
+    elif intent_name == "A_GetJobScope - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobScope - no":
+        resp_text = "Okay what else can I do for you?"       
+    elif intent_name == "A_GetJobYears":
+        persona = "Go Getter"
+        jobInterestedIn = req["queryResult"]["parameters"]["jobtitle"]
+        #years = getJobMinYearsExperience(jobInterestedIn)
+        resp_text =  f"{jobInterestedIn} typically requires {years} years of experience. Would you be interested to see a road map on how to get there?"
+    elif intent_name == "A_GetJobYears - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetJobYears - no":
+        resp_text = "Okay what else can I do for you?"          
+    elif intent_name == "A_GetServicesInfo":
+        persona = "Curious Explorer"
+        resp_text = "I can help you develop a personalised career road map and help you look for suitable jobs and training courses. Would you like to give it a go?"
+    elif intent_name == "A_GetServicesInfo - yes":
+        resp_text = "Great! First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_GetServicesInfo - no":
+        resp_text = "Okay what else can I do for you?"     
+    elif intent_name == "A_LookforCareerPath":
+        persona = "Curious Explorer"
+        resp_text = "I can help you develop a personalised career road map. First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_LookforJob":
+        persona = "Unemployed Job Seeker"
+        resp_text = "I know, its tough finding a job these days. Let me help you find a suitable job! First, I need to know what was your last position and how long you had been in it?"
+    elif intent_name == "A_LookforJobChange":
+        persona = "Jaded Employee"
+        resp_text = "I am sorry to hear that. I think I can help you. First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "A_LookforSelfImprovement":
+        persona = "Eager Learner"
+        resp_text = "I am glad you are actively seeking to improve yourself. I can help you with that. First, I need to know what is your current position and how long you have been in it?"
+    elif intent_name == "ShowCareerRoadMap":
+        if persona == "Jaded Employee" or persona == "Go Getter" or persona == "Curious Explorer":
+            resp_text = "Now lets find out what competencies you have"
+        elif persona == "Unemployed Job Seeker":
+            resp_text = "This is the list of Course Recommendations"
+        elif persona == "Eager Learner":
+            resp_text = "This is the list of Job Recommendations"
+    elif intent_name =="GivePositionDetails":
+        currentPosition = req["queryResult"]["parameters"]["currentPosition"]
+        yearsOfWokringExperience = req["queryResult"]["parameters"]["yearsOfWokringExperience"]
+        if persona == "Unemployed Job Seeker":
+            resp_text = "I have a found a few potential jobs for you. To narrow the search, I would need you to select the competencies that you have."
+        elif persona == "Jaded Employee":
+            resp_text = "Do you have any pinnacle position in mind?"
+    elif intent_name =="GiveEmailAddress":
+        emailAddress = req["queryResult"]["parameters"]["emailAddress"]
+
+
+####
+# DialogFlow Block : Dom
+####
+
+    # D_ElicitEmployDetails Intent
+    if intent_name == "D_ElicitEmployDetails":
+        jobtitle = req["queryResult"]["parameters"]["jobtitle"]
+        yearsInCurrentPosition = req["queryResult"]["parameters"]["duration"]
+
+        if persona == "Jaded Employee" or persona == "Curious Explorer":
+            #Lead to D_GetCareerPreferences Intent
+            resp_text = "D_ElicitEmployDetails:JECE - What kind of job roles do you prefer? Management or technical track?"
+        if persona == "Unemployed Job Seeker" or persona == "Eager Learner":
+            #Lead to D_ElicitEmployDetails - yes Intent
+            resp_text = "D_ElicitEmployDetails:UJS - I have noted your employment details. Next, would you share with me more about your competency?"
+        if persona == "Go Getter":
+            #Lead to D_GetAspiration Intent
+            resp_text = "D_ElicitEmployDetails:GG - I have noted your employment details. Now, if given an opportunity, what do you aspire to be? CIO?"
+    elif intent_name == "D_ElicitEmployDetails - yes":
+        if persona == "Unemployed Job Seeker" or persona == "Eager Learner":
+            #Lead to Competency Intent
+            resp_text = "D_ElicitEmployDetails - yes:UJSEL That's great. Based on the following list, please key in your relevant competencies."
+    elif intent_name == "D_ElicitEmployDetails - no":
+        resp_text = "D_ElicitEmployDetails - no - Erm, alright. Is there anything else that I can help you?"
     
+    # D_GetCareerPreferences Intent
+    elif intent_name == "D_GetCareerPreferences":
+        #Lead to D_GetAspiration Intent
+        resp_text = "D_GetCareerPreferences - If given an opportunity, what do you aspire to be? CIO?"
+    elif intent_name == "D_GetCareerPreferences - yes":
+        #Lead to Competency Intent
+        resp_text = "D_GetCareerPreferences - yes - Great to hear that. Based on the following list, please key in your relevant competencies."
+    elif intent_name == "D_GetCareerPreferences - no":
+        resp_text = "D_GetCareerPreferences - no - That's cool. Is there any help that I can render to you?"
+    
+    # D_GetAspiration Intent
+    elif intent_name == "D_GetAspiration":
+        #Lead to D_GetAspiration - yes Intent
+        aspiredjobtitle = req["queryResult"]["parameters"]["jobtitle"]
+        resp_text = "D_GetAspiration - This is your career road map."
+        #resp_text = getCareerPath(jobtitle, aspiredjobtitle)
+        resp_text = resp_text + "I think I can value add more in terms of career advice. Would you like to share more about your competency?"
+    elif intent_name == "D_GetAspiration - yes":
+        #Lead to Competency Intent
+        resp_text = "D_GetAspiration - yes - Great to hear that. Based on the following list, please key in your relevant competencies."
+    elif intent_name == "D_GetAspiration - no":
+        resp_text = "D_GetAspiration - no - That's alright. Is there anything that I can assist you?"
+
 # **********************
 # DialogFlow block : START_KENNETH
 # **********************
