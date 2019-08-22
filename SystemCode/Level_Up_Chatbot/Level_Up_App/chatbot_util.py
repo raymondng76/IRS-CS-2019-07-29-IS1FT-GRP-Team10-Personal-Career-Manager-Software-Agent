4e59fbb7c5d18d789a6efdb246a396bc4d42591b
Raymond <35454616+raymondng76@users.noreply.github.com>
2019-08-22 10:22:22 +0800

    Fix job recommendation function

--- SystemCode/Level_Up_Chatbot/Level_Up_App/chatbot_util.py
+++ SystemCode/Level_Up_Chatbot/Level_Up_App/chatbot_util.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from Level_Up_App.models import CareerSkills, CareerPosition, Skill, Job, GenericInfo, CareerPathMap
 from Level_Up_App.courserecommendationrules import CourseRecommender, SkillGapsFact, recommendedcourses
-from Level_Up_App.jobrecommendationrules import JobRecommender, SkillSetFact, recommendedjobs
+from Level_Up_App.jobrecommendationrules import getJobRecommendation
 from Level_Up_App.genericjobinfo import *
 from Level_Up_App.careerknowledgegraph import *
 from Level_Up_App.CareerPathASTARSearch import *
@@ -70,23 +70,16 @@ def elicit_competence_without_endgoal(currPos):
 def jobsrecommendation_with_endgoal(currPos, endGoal, userCompetence):
     if not userCompetence:
         return list()
-    origialCompetenceList = elicit_competence_with_endgoal(currPos, endGoal)
-    origialCompetenceList.append(userCompetence)
-    return getJobRecommendation(origialCompetenceList)
+    competenceList = elicit_competence_with_endgoal(currPos, endGoal)
+    competenceList.append(userCompetence)
+    return getJobRecommendation(competenceList)
 
 def jobsrecommendation_without_endgoal(currPos, userCompetence):
     if not userCompetence:
         return list()
-    origialCompetenceList = elicit_competence_without_endgoal(currPos)
-    origialCompetenceList.append(userCompetence)
-    return getJobRecommendation(origialCompetenceList)
-
-def getJobRecommendation(skillset):
-    engine = JobRecommender()
-    engine.reset()
-    engine.declare(SkillSetFact(skills=skillset))
-    engine.run()
-    return recommendedjobs
+    competenceList = elicit_competence_without_endgoal(currPos)
+    competenceList.append(userCompetence)
+    return getJobRecommendation(competenceList)
 #****************************************
 # Methods for jobs recommendation : END
 #****************************************
@@ -158,96 +151,3 @@ def getCareerPathMap(currPos): # Input is string
     # Get career path map object filter by career pos object
     careerPath = CareerPathMap.objects.filter(initialpos=currCareerPos)
     return careerPath # This is a queryset of careerpath
-
-def getJobDescriptionTemp(jobtitle):
-    title = jobtitle.lower()
-    if title == 'intern':
-        return getInternDescription()
-    if title == 'engineer':
-        return getEngineerDescription()
-    if title == 'senior engineer':
-        return getSeniorEngineerDescription()
-    if title == 'software engineer':
-        return getSoftwareEngineerDescription()
-    if title == 'software developer':
-        return getSoftwareDeveloperDescription()
-    if title == 'analyst':
-        return getAnalystDescription()
-    if title == 'sales engineer':
-        return getSalesEngineerDescription()
-    if title == 'business analyst':
-        return getBusinessAnalystDescription()
-    if title == 'principle engineer':
-        return getPrincipalEngineerDescription()
-    if title == 'senior software engineer':
-        return getSeniorSoftwareEngineerDescription()
-    if title == 'senior software developer':
-        return getSeniorSoftwareDeveloperDescription()
-    if title == 'solution architect':
-        return getSolutionArchitectDescription()
-    if title == 'consultant':
-        return getConsultantDescription()
-    if title == 'senior analyst':
-        return getSeniorAnalystDescription()
-    if title == 'senior sales engineer':
-        return getSeniorSalesEngineerDescription()
-    if title == 'senior business analyst':
-        return getSeniorBusinessAnalystDescription()
-    if title == 'principle software engineer':
-        return getPrincipalSoftwareEngineerDescription()
-    if title == 'senior solution architect':
-        return getSeniorSolutionArchitectDescription()
-    if title == 'senior consultant':
-        return getSeniorConsultantDescription()
-    if title == 'technical lead':
-        return getTechnicalLeadDescription()
-    if title == 'project manager':
-        return getProjectManagerDescription()
-    if title == 'software manager':
-        return getSoftwareManagerDescription()
-    if title == 'sales manager':
-        return getSalesManagerDescription()
-    if title == 'senior project manager':
-        return getSeniorProjectManagerDescription()
-    if title == 'technical manager':
-        return getTechnicalManagerDescription()
-    if title == 'business manager':
-        return getBusinessManagerDescription()
-    if title == 'senior software manager':
-        return getSeniorSoftwareManagerDescription()
-    if title == 'senior sales manager':
-        return getSeniorSalesManagerDescription()
-    if title == 'senior technical manager':
-        return getSeniorTechnicalManagerDescription()
-    if title == 'programme manager':
-        return getProgrammeManagerDescription()
-    if title == 'sales director':
-        return getSalesDirectorDescription()
-    if title == 'professor':
-        return getProfessorDescription()
-    if title == 'director':
-        return getDirectorDescription()
-    if title == 'software director':
-        return getSoftwareDirectorDescription()
-    if title == 'head':
-        return getHeadDescription()
-    if title == 'senior sales director':
-        return getSeniorSalesDirectorDescription()
-    if title == 'senior director':
-        return getSeniorDirectorDescription()
-    if title == 'senior software director':
-        return getSeniorSoftwareDirectorDescription()
-    if title == 'senior head':
-        return getSeniorHeadDescription()
-    if title == 'chief technology officer' or title == 'cto':
-        return getCTODescription()
-    if title == 'vice president' or title == 'vp':
-        return getVPDescription()
-    if title == 'chief information officer' or title == 'cio':
-        return getCIODescription()
-    if title == 'president':
-        return getPresidentDescription()
-    if title == 'chief operating officer' or title == 'coo':
-        return getCOODescription()
-    if title == 'chief executive officer' or title == 'ceo':
-        return getCEODescription()
